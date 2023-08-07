2e2e171754ec4efd4bbb9ceb7c6697bfcd5826bb
paintparisut <92552125+paintparisut@users.noreply.github.com>
2023-08-07 14:31:54 +0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -14,10 +14,10 @@ terraform {
 }
 
 provider "azurerm" {
-#  subscription_id  =  "<subscription-id>"
-#  tenant_id  =  "<tenant-id>"  
-#  client_id  =  "<client-id>"
-#  client_secret  =  "<client-secret>"
+  subscription_id  =  "<subscription-id>"
+  tenant_id  =  "<tenant-id>"  
+  client_id  =  "<client-id>"
+  client_secret  =  "<client-secret>"
   features {}
 }
 
