4b1875bc5192e5e995e3a3ccd0c6895b028ca504
natthidakl <140678535+natthidakl@users.noreply.github.com>
2023-08-08 11:11:54 +0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -13,11 +13,16 @@ terraform {
   #}
 }
 
+variable "ARM_SUBSCRIPTION_ID" {}
+variable "ARM_TENANT_ID" {}
+variable "ARM_CLIENT_ID" {}
+variable "ARM_CLIENT_SECRET" {}
+
 provider "azurerm" {
-  subscription_id  =  "${env.ARM_SUBSCRIPTION_ID}" 
-  tenant_id  =  "${env.ARM_TENANT_ID}"  
-  client_id  =  "${env.ARM_CLIENT_ID}"
-  client_secret  =  "${env.ARM_CLIENT_SECRET}"
+  subscription_id  =  "${var.ARM_SUBSCRIPTION_ID}" 
+  tenant_id  =  "${var.ARM_TENANT_ID}"  
+  client_id  =  "${var.ARM_CLIENT_ID}"
+  client_secret  =  "${var.ARM_CLIENT_SECRET}"
   features {}
 }
 
