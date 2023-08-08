16f14062a14e53ddc59c0a20476f465a852af738
Suttipan Tamluek <suttipan.tamluek@scbtechx.io>
2023-08-08 15:18:19 +0700

    edit

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {  
   backend  "azurerm" {  
-   resource_group_name  = "rg-ais-payment-gateway"
+    resource_group_name  = "rg-ais-payment-gateway"
     storage_account_name = "sbpocstoacc"
     container_name       = "tfstatetest"
     key                  = "terraform.tfstate"
