ab9a4a7b18d2058f8ff1a79774c2f172d6064700
Suttipan Tamluek <suttipan.tamluek@scbtechx.io>
2023-08-08 14:25:07 +0700

    rearrange

---
+++ backend.tf
@@ -0,0 +1,8 @@
+terraform {  
+  backend  "azurerm" {  
+   resource_group_name  = "rg-ais-payment-gateway"
+    storage_account_name = "sbpocstoacc"
+    container_name       = "tfstatetest"
+    key                  = "terraform.tfstate"
+  }
+}
