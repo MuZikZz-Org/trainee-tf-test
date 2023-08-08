ff191bc52c86e848f0a37c6701d803efde77dc86
Suttipan Tamluek <suttipan.tamluek@scbtechx.io>
2023-08-08 15:08:50 +0700

    add skip_provider_registration

--- provider.tf
+++ provider.tf
@@ -8,9 +8,12 @@ terraform {
 }
 
 provider "azurerm" {
+  features {
+    skip_provider_registration = true
+  }
+  
   subscription_id   =   var.ARM_SUBSCRIPTION_ID
   tenant_id         =   var.ARM_TENANT_ID  
   client_id         =   var.ARM_CLIENT_ID
   client_secret     =   var.ARM_CLIENT_SECRET
-  features {}
 }
