194dc6ed47bfcce4365860e25fbebb6c66bde6ab
thunsudamn <140685871+thunsudamn@users.noreply.github.com>
2023-08-15 15:13:52 +0700

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -2,3 +2,8 @@ variable "ARM_SUBSCRIPTION_ID" {}
 variable "ARM_TENANT_ID" {}
 variable "ARM_CLIENT_ID" {}
 variable "ARM_CLIENT_SECRET" {}
+
+variable "SRC_IMG_REF_PUBLISHER" {}
+variable "SRC_IMG_REF_OFFER" {}
+variable "SRC_IMG_REF_SKU" {}
+variable "SRC_IMG_REF_VERSION" {}
