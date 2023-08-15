7b4afbc859327c66ed6ed0ac326e61c42da7ffbb
natthidakl <140678535+natthidakl@users.noreply.github.com>
2023-08-15 17:05:21 +0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -138,7 +138,7 @@ resource "azurerm_linux_virtual_machine" "my_terraform_vm" {
     connection {
       type        = "ssh"
       user        = "natthidak"
-      host        = azurerm_public_ip.my_terraform_public_ip
+      host        = azurerm_public_ip.my_terraform_public_ip.id
       private_key = "NatthidaK@16"
     }
      script = "provisioner.sh"
