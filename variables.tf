1254ebefc91d86a4ebd1a996cf52b4b5caf66ea9
natthidakl <140678535+natthidakl@users.noreply.github.com>
2023-08-15 11:05:05 +0700

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,4 +1,12 @@
-variable "ARM_SUBSCRIPTION_ID" {}
-variable "ARM_TENANT_ID" {}
-variable "ARM_CLIENT_ID" {}
-variable "ARM_CLIENT_SECRET" {}
+variable "ARM_SUBSCRIPTION_ID" {
+  default = var.SET_ARM_SUBSCRIPTION_ID
+}
+variable "ARM_TENANT_ID" {
+  default = var.SET_ARM_TENANT_ID
+}
+variable "ARM_CLIENT_ID" {
+  default = var.SET_ARM_CLIEND_ID
+}
+variable "ARM_CLIENT_SECRET" {
+  default = var.SET_ARM_CLIENT_SECRET
+}
