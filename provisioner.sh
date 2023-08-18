b5aaa19b356f31826982a1a10ece55abd056ee2b
natthidakl <140678535+natthidakl@users.noreply.github.com>
2023-08-18 14:07:54 +0700

    Update provisioner.sh

--- provisioner.sh
+++ provisioner.sh
@@ -4,11 +4,11 @@ sudo apt-get update
 echo "update done"
 # sudo apt-get -y upgrade
 # echo "upgrade done"
-sudo apt install openjdk-11-jdk
+sudo apt-get install openjdk-11-jdk
 echo "install openjdk done"
 wget https://binaries.sonarsource.com/Distribution/sonarqube/sonarqube-9.0.1.46107.zip
-echo "install sonarqube done"
-sudo apt install unzip
+echo "get sonarqube done"
+sudo apt-get install unzip
 echo "install unzip done"
 unzip sonarqube-9.0.1.46107.zip
 echo "unzip done"
